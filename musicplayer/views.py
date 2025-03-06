a0f1d5076211a0fcae4e0e2a1f5e7dbd0eb36a74
abdulmoizniazi <abdulmoizniazi@gmail.com>
2025-03-07 01:32:10 +0500

    first commit by self

---
+++ musicplayer/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Song
+from django.core.paginator import Paginator
+
+# Create your views here.
+
+
+def index(request):
+    # return HttpResponse("hogya kia??")
+
+    paginator = Paginator(Song.objects.all(), 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
+    return render(request, 'index.html', context)
